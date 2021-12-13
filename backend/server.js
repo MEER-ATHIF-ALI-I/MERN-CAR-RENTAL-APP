7b33e0b5f3d407a3f341ef0adfedcd7e42ad6b8a
MEER ATHIF ALI I <meerathifali@gmail.com>
2021-12-14 02:39:57 +0530

    comit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const dbConnection = require('./db')
+const usersRoute = require('./routes/usersRoute')
+const carsRoute = require('./routes/carsRoute')
+const bookingsRoute = require('./routes/bookingsRoute')
+
 app.use(express.json());
 
 app.use("/api/cars/", require("./routes/carsRoute"));
