b9eb45b0580871a13d5a4d47d2509440e289d9f3
MEER ATHIF ALI I <meerathifali@gmail.com>
2021-12-13 20:09:25 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoURL = process.env.MONGODB_URL
+
+mongoose.connect(mongoURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var db = mongoose.connection
+
+db.on('connected' , ()=>{
+    console.log('Mongo DB Connection Successfull');
+})
+
+db.on('error' , ()=>{
+    console.log(`Mongo DB Connection failed`);
+})
+
+module.exports =mongoose
