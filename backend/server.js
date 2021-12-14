f61b94605e34fb9464b48f1ce496fbba34aac072
MEER ATHIF ALI I <meerathifali@gmail.com>
2021-12-14 15:30:58 +0530

    scripts added

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,41 @@
 const express = require("express");
 const app = express();
+const Car = require('./models/carModel');
 const port = process.env.PORT || 5000;
-const dbConnection = require('./db')
+const dbConnection = require ('./db');
+app.use(express.json());
+const path = require("path");
 const usersRoute = require('./routes/usersRoute')
 const carsRoute = require('./routes/carsRoute')
 const bookingsRoute = require('./routes/bookingsRoute')
 
-app.use(express.json());
+
 
 app.use("/api/cars/", require("./routes/carsRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
 
-const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("frontend/build"));
+//----------deployment-----------------
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
-  });
+__dirname = path.resolve();
+
+if(process.env.NODE_ENV==="production") {
+ app.use(express.static(path.join(__dirname,"/frontend/build")));
+
+ app.get('*',(req,res)=>{
+   res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+ });
+}else{
+	app.get("/", (req, res)=>{
+      res.send("API is running..");
+	});
 }
+
+
+//----------deployment-----------
+
+
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
