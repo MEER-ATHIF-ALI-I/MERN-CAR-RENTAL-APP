2faab54734b4906b1f1c8e7da2dd04a746495d1c
MEER ATHIF ALI I <85889726+MEER-ATHIF-ALI-I@users.noreply.github.com>
2024-07-18 18:22:23 +0530

    url changes

--- frontend/src/redux/actions/carsActions.js
+++ frontend/src/redux/actions/carsActions.js
@@ -6,7 +6,7 @@ export const getAllCars=()=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-        const response = await axios.get('/api/cars/getallcars')
+        const response = await axios.get('https://mern-car-rental-app.onrender.com/api/cars/getallcars')
         dispatch({type: 'GET_ALL_CARS', payload:response.data})
         dispatch({type: 'LOADING' , payload:false})
     } catch (error) {
@@ -21,7 +21,7 @@ export const addCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/addcar' , reqObj)
+         await axios.post('https://mern-car-rental-app.onrender.com/api/cars/addcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('New car added successfully')
@@ -41,7 +41,7 @@ export const editCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/editcar' , reqObj)
+         await axios.post('https://mern-car-rental-app.onrender.com/api/cars/editcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('Car details updated successfully')
@@ -61,7 +61,7 @@ export const deleteCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/deletecar' , reqObj)
+         await axios.post('https://mern-car-rental-app.onrender.com/api/cars/deletecar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('Car deleted successfully')
